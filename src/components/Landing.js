b251e63fe480af8debb7921e48d9c2fed05a1f42
Carolina Brown <carolinaxcury@gmail.com>
2018-08-27 13:35:21 -0400

    landing css done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,46 @@
 import React from 'react';
+import '.././styles/Landing.css';
+
 
 const Landing = () => (
-  <section className="landing">
-    <h1 className="hero-title">Turn the music up!</h1>
-    
-    <section className="selling-points">
-      <div className="point">
-        <h2 className="point-title">Choose your music</h2>
+  
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+</head>
+
+<body>
+   <section className="landing">
+
+     <div className="banner">
+            <img className="banner-img" src='assets/images/music-logo.jpg' alt="Bloc AM Banner" />
+            <h1 className="hero-title"> Turn the music up!</h1>
+        </div>
+
+     <section className="selling-points">
+           <div id="firstbox" className="point">
+                  <span class="glyphicon">&#xe002;</span>
+              
+                 <h2 className="point-title">Choose your music</h2>
         <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
       </div>
-      <div className="point">
-        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+      <div id="secondbox" className="point">
+        <span class="glyphicon">&#xe018;</span>
+        <h2 className="point-title">Unlimited streaming, ad-free</h2>
         <p className="point-description">No arbitrary limits. No distractions.</p>
       </div>
-      <div className="point">
+      <div id="thirdbox" className="point">
+      <span class="glyphicon">&#xe145;</span>
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </div>
+    
     </section>
+
   </section>
+  </body>
+  </html>
+
 );
 
 export default Landing;
